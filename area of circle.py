1c21d034271748bcd833e62af3a7e926e45d446a
ghosted123a <61979494+ghost123a@users.noreply.github.com>
2020-03-25 21:15:49 +0000

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,7 @@
+print('calculate the area of a circle')
+radius = input('radius: ')
+R = int(radius)**2
+PI = 3.14
+area_of_circle = PI * R
+print('the area of the circle is')
+print(area_of_circle)
